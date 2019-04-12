31b4b65b9b89a7ab807c376718870651d427b3dd
kikuchi_tomoki <t.kikuchi@snowrobin.jp>
2019-04-12 18:11:04 +0900

    IEのときだけポリフィルを有効にする

--- src/stickyfill/js/index.js
+++ src/stickyfill/js/index.js
@@ -1,6 +1,13 @@
+import UAParser from 'ua-parser-js';
 import * as Stickyfill from 'stickyfilljs';
 
+const parser = new UAParser();
+const isIe = parser.getBrowser().name === 'IE';
+
 document.addEventListener('DOMContentLoaded', () => {
-  const stickyTarget = document.querySelector('.menu');
-  Stickyfill.add(stickyTarget);
+  if (isIe) {
+    console.log('IEだよ');
+    const stickyTarget = document.querySelector('.menu');
+    Stickyfill.add(stickyTarget);
+  }
 });
